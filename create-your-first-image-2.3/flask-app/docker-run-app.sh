90a161793cf981b2cae06fa19aabc39d7ca7d031
mjlivingston40 <matt.j.livingston.40@gmail.com>
2020-05-01 14:23:57 -0400

    create-your-first-image-2.3/flask-app/docker-run-app.sh

---
+++ create-your-first-image-2.3/flask-app/docker-run-app.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+imageName=$1
+author=$2
+tagName=$3
+containerName=$4
+
+echo Actually runs on http://localhost:8888
+
+docker run -p 8888:5000 --name ${containerName} ${author}/${imageName}:${tagName}
+
